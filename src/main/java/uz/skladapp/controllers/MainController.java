b355314b8d1656050a653711ddbde981e8c61adc
ravshanmakhmadaliev <ravshan9779@mail.ru>
2019-03-16 19:24:22 +0500

    first commit

--- src/main/java/uz/skladapp/controllers/MainController.java
+++ src/main/java/uz/skladapp/controllers/MainController.java
@@ -1,14 +1,15 @@
 package uz.skladapp.controllers;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uz.skladapp.model.Company;
 import uz.skladapp.repositories.CompanyRepository;
 
+
+import java.security.Key;
 import java.util.Optional;
 
 @RestController
@@ -19,15 +20,47 @@ public class MainController {
     private CompanyRepository companyRepository;
 
     @RequestMapping("/companies")
-    public @ResponseBody Iterable<Company> getList() {
+    public @ResponseBody
+    Iterable<Company> getList() {
         return companyRepository.findAll();
     }
 
     @RequestMapping("/companies/")
-    public Optional<Company> getCompany(@RequestParam(value="id", defaultValue="1") String id) {
+    public Optional<Company> getCompany(@RequestParam(value = "id", defaultValue = "1") String id) {
+
+
+        // We need a signing key, so we'll create one just for this example. Usually
+        // the key would be read from your application configuration instead.
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+        String jws = Jwts.builder()
+
+                .setSubject("{\'username\':\'root\',\'password\':\'root\'}")
+
+                .signWith(key)
+
+                .compact();
+
+        System.out.println("this is " + jws);
+
+        try {
+            Jws<Claims> a=Jwts.parser().setSigningKey(key).parseClaimsJws(jws);
+            String text = a.getBody().getSubject();
+            System.out.println("hti is fine\n"+text);
+            //OK, we can trust this JWT
+
+        } catch (JwtException e) {
+
+            System.out.println(e.getMessage());
+            //don't trust the JWT!
+        }
         return companyRepository.findById(Long.valueOf(id));
     }
 
+    @PostMapping("/company")
+    public void saveCompany(@RequestBody Company company) {
+        companyRepository.save(company);
+    }
 
 
 }
