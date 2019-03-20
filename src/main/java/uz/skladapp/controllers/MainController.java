b21dbd0122e421c6e299ad87470a5519cebc16b6
Mirzogolib <mirzogolib96@gmail.com>
2019-03-20 11:18:15 +0500

    Migo commit -> All models (without Many to many)

--- src/main/java/uz/skladapp/controllers/MainController.java
+++ src/main/java/uz/skladapp/controllers/MainController.java
@@ -7,16 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
-import uz.skladapp.model.Category;
-import uz.skladapp.model.Company;
-import uz.skladapp.repositories.CategoryRepository;
-import uz.skladapp.repositories.CompanyRepository;
+import uz.skladapp.model.*;
+import uz.skladapp.repositories.*;
 
 
 import java.security.Key;
 import java.util.Optional;
 
 @RestController
+//@Transactional(propagation = Propagation.REQUIRES_NEW)
+//@EnableAutoConfiguration(exclude = HypermediaAutoConfiguration.class)
+//@EnableAspectJAutoProxy
 public class MainController {
 
 
@@ -48,9 +49,9 @@ public class MainController {
         System.out.println("this is " + jws);
 
         try {
-            Jws<Claims> a=Jwts.parser().setSigningKey(key).parseClaimsJws(jws);
+            Jws<Claims> a = Jwts.parser().setSigningKey(key).parseClaimsJws(jws);
             String text = a.getBody().getSubject();
-            System.out.println("hti is fine\n"+text);
+            System.out.println("hti is fine\n" + text);
             //OK, we can trust this JWT
 
         } catch (JwtException e) {
@@ -74,14 +75,144 @@ public class MainController {
 
     @RequestMapping("/categories")
     public @ResponseBody
-    Page<Category> getList(@RequestParam(value = "page") String page) {
+    Page<Category> getCategoryList(@RequestParam(value = "page") String page) {
         return categoryRepository.findAll(PageRequest.of(Integer.valueOf(page), 2));
     }
 
-    @RequestMapping("/categories/")
-    public Optional<Category> getCategory(@RequestParam(value = "id", defaultValue = "0") String id) {
+    //@PathVariable("id") String id --> {id} --> works without __link__ and other staff ))
+    @RequestMapping("/categories/{id}")
+    public Optional<Category> getCategory(@PathVariable("id") String id) {
         return categoryRepository.findById(Long.valueOf(id));
     }
     //end migo
+
+    //migo Role
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @RequestMapping("/roles")
+    public @ResponseBody
+    Iterable<Role> getRoleList() {
+        return roleRepository.findAll();
+    }
+
+    @RequestMapping("/roles/{id}")
+    public Optional<Role> getRole(@PathVariable("id") String id) {
+        return roleRepository.findById(Long.valueOf(id));
+    }
+    //migo User
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping("/users")
+    public @ResponseBody
+    Iterable<User> getUserList() {
+        return userRepository.findAll();
+    }
+
+    @RequestMapping("/users/{id}")
+    public Optional<User> getUser(@PathVariable("id") String id) {
+        return userRepository.findById(Long.valueOf(id));
+    }
+
+    //migo Product
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @RequestMapping("/products")
+    public @ResponseBody
+    Iterable<Product> getProductList() {
+        return productRepository.findAll();
+    }
+
+    @RequestMapping("/products/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") String id) {
+        return productRepository.findById(Long.valueOf(id));
+    }
+
+    //migo Attributes
+
+    @Autowired
+    private AttributeRepository attributeRepository;
+
+    @RequestMapping("/attributes")
+    public @ResponseBody
+    Iterable<Attributes> getAttributeList() {
+        return attributeRepository.findAll();
+    }
+
+    @RequestMapping("/attributes/{id}")
+    public Optional<Attributes> getAttributes(@PathVariable("id") String id) {
+        return attributeRepository.findById(Long.valueOf(id));
+    }
+
+    //migo Attributes
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @RequestMapping("/departments")
+    public @ResponseBody
+    Iterable<Department> getDepartmentList() {
+        return departmentRepository.findAll();
+    }
+
+    @RequestMapping("/departments/{id}")
+    public Optional<Department> getDepartment(@PathVariable("id") String id) {
+        return departmentRepository.findById(Long.valueOf(id));
+    }
+
+    //migo Storage
+
+    @Autowired
+    private StorageRepository storageRepository;
+
+    @RequestMapping("/storages")
+    public @ResponseBody
+    Iterable<Storage> getStorageList() {
+        return storageRepository.findAll();
+    }
+
+    @RequestMapping("/storages/{id}")
+    public Optional<Storage> getStorage(@PathVariable("id") String id) {
+        return storageRepository.findById(Long.valueOf(id));
+    }
+
+    //migo Storage_Product
+
+//    @Autowired
+//    private Storage_ProductRepository storage_productRepository;
+
+//    @RequestMapping("/storage_product")
+//    public @ResponseBody
+//    Iterable<Storage_Product> getStorageProductList() {
+//        return storage_productRepository.findAll();
+//    }
+
+//    @RequestMapping("/storage_product/")
+//    public Optional<Storage_Product> getStorageProduct(@RequestParam(value = "id", defaultValue = "0") String id) {
+//        return storageRepository.findById(Long.valueOf(id));
+//    }
+
+    //migo Storage
+
+    @Autowired
+    private PermissionRepository permissionRepository;
+
+    @RequestMapping("/permission")
+    public @ResponseBody
+    Iterable<Permission> getPermissionList() {
+        return permissionRepository.findAll();
+    }
+
+    @RequestMapping("/permission/{id}")
+    public Optional<Permission> getPermission(@PathVariable("id") String id) {
+        return permissionRepository.findById(Long.valueOf(id));
+    }
+
+
 
 }
--- src/main/java/uz/skladapp/model/Company.java
+++ src/main/java/uz/skladapp/model/Company.java
@@ -1,11 +1,16 @@
 package uz.skladapp.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,43 +22,4 @@ public class Company {
     private String company_phone;
 
 
-    public long getCompany_ID() {
-        return company_ID;
-    }
-
-    public void setCompany_ID(long company_ID) {
-        this.company_ID = company_ID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLogo() {
-        return logo;
-    }
-
-    public void setLogo(String logo) {
-        this.logo = logo;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return company_phone;
-    }
-
-    public void setPhone(String phone) {
-        this.company_phone = phone;
-    }
 }
